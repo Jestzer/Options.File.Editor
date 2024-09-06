c9dedecd9ee80d5234b95af01475198200ad4741
Jestzer <james@buonopane.email>
2024-09-06 17:13:51 -0400

    Made some progress, I guess.

--- Options.File.Editor/ViewModels/MainViewModel.cs
+++ Options.File.Editor/ViewModels/MainViewModel.cs
@@ -1,13 +1,66 @@
-﻿namespace Options.File.Editor.ViewModels;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public partial class MainViewModel : ViewModelBase
+namespace Options.File.Editor.ViewModels
 {
-    public static string PackageVersion => GetPackageVersion();
-
-    private static string GetPackageVersion()
+    public class MainViewModel : INotifyPropertyChanged
     {
-        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-        var version = assembly?.GetName().Version?.ToString();
-        return version ?? "Error getting version number.";
+        public static string PackageVersion => GetPackageVersion();
+
+        private static string GetPackageVersion()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var version = assembly?.GetName().Version?.ToString();
+            return version ?? "Error getting version number.";
+        }
+
+        public ObservableCollection<string> FirstListItems { get; } = new ObservableCollection<string>
+        {
+            "Item 1",
+            "Item 2",
+            "Item 3"
+        };
+
+        private string _selectedFirstItem;
+        public string SelectedFirstItem
+        {
+            get => _selectedFirstItem;
+            set
+            {
+                if (_selectedFirstItem != value)
+                {
+                    _selectedFirstItem = value;
+                    OnPropertyChanged();
+                    UpdateSecondListItems();
+                }
+            }
+        }
+
+        public ObservableCollection<string> SecondListItems { get; } = new ObservableCollection<string>();
+
+        private void UpdateSecondListItems()
+        {
+            SecondListItems.Clear();
+            if (SelectedFirstItem == "Item 1")
+            {
+                SecondListItems.Add("Subitem A");
+                SecondListItems.Add("Subitem B");
+                SecondListItems.Add("Subitem C");
+            }
+            else if (SelectedFirstItem == "Item 2")
+            {
+                SecondListItems.Add("Subitem D");
+                SecondListItems.Add("Subitem E");
+                SecondListItems.Add("Subitem F");
+            }
+            // Add more conditions as needed
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
